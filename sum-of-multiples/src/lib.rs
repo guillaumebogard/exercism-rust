365e179fd63125c8484a68d892c8a7857a59fb40
Guillaume Bogard <guillaume.bogardcoquard@gmail.com>
2023-01-28 14:00:04 -0800

    exercise corrected: sum-of-multiples

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -3,7 +3,7 @@ pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
 
   (1..limit).for_each(|x| {
     if factors.iter().any(|factor| *factor != 0 && x % factor == 0) {
-      sum += n;
+      sum += x;
     }
   });
 
