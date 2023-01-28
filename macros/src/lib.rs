a231454c24e3ddd38b762dbbb7e9e85bc2aff8f8
Guillaume Bogard <guillaume.bogardcoquard@gmail.com>
2023-01-28 14:10:46 -0800

    exercise done: macros

---
+++ macros/src/lib.rs
@@ -0,0 +1,26 @@
+#[macro_export]
+macro_rules! hashmap {
+  () => {
+    ::std::collections::HashMap::new()
+  };
+
+  ($($key:expr => $value:expr),*) => {
+    {
+      let mut map = ::std::collections::HashMap::new();
+      $(
+        map.insert($key, $value);
+      )*
+      map
+    }
+  };
+
+  ($($key:expr => $value:expr,)*) => {
+    {
+      let mut map = ::std::collections::HashMap::new();
+      $(
+        map.insert($key, $value);
+      )*
+      map
+    }
+  };
+}
