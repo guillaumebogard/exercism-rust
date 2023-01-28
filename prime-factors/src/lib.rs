cf1fd683ef1014d4f62c50ce759c2ee9f7beccd0
Guillaume Bogard <guillaume.bogardcoquard@gmail.com>
2023-01-28 13:09:53 -0800

    exercise done: prime-factors

---
+++ prime-factors/src/lib.rs
@@ -0,0 +1,16 @@
+pub fn factors(n: u64) -> Vec<u64> {
+  let mut prime_factors = vec![];
+  let mut n = n;
+  let mut i = 2;
+
+  while i <= n {
+    if n % i == 0 {
+      prime_factors.push(i);
+      n /= i;
+    } else {
+      i += 1;
+    }
+  }
+
+  prime_factors
+}
