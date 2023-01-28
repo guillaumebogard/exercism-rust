7a0140414c096122ee151dbbf839e75f000163e9
Guillaume Bogard <guillaume.bogardcoquard@gmail.com>
2023-01-28 13:58:19 -0800

    exercise done: high-scores

---
+++ high-scores/src/lib.rs
@@ -0,0 +1,37 @@
+#[derive(Debug)]
+pub struct HighScores {
+  scores: Vec<u32>
+}
+
+impl HighScores {
+  pub fn new(scores: &[u32]) -> Self {
+    HighScores { scores: scores.to_vec() }
+  }
+
+  pub fn scores(&self) -> &[u32] {
+    &self.scores
+  }
+
+  pub fn latest(&self) -> Option<u32> {
+    self.scores
+      .last()
+      .copied()
+  }
+
+  pub fn personal_best(&self) -> Option<u32> {
+    self.scores
+      .iter()
+      .max()
+      .copied()
+  }
+
+  pub fn personal_top_three(&self) -> Vec<u32> {
+    let mut personal_top_three = self.scores.clone();
+
+    personal_top_three.sort();
+    personal_top_three.reverse();
+    personal_top_three.truncate(3);
+
+    personal_top_three
+  }
+}
